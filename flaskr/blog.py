24c36958fae88f1b90be7476ccd8860de9534c2b
Fernando Souza <fernandostorm1@gmail.com>
2018-05-09 21:29:43 -0300

    Added route and template for the blog page

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,7 +1,7 @@
 import functools
 
 from flask import (
-    Blueprint, flask, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for
 )
 
 from werkzeug.exceptions import abort
@@ -10,3 +10,13 @@ from  flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetch()
+    return render_template('blog/index.html', posts=posts)
