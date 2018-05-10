68e906f6e70c38087b9e713fbdfcc20df32ffd97
Fernando Souza <fernandostorm1@gmail.com>
2018-05-09 21:04:37 -0300

    Blog blueprint included

---
+++ flaskr/blog.py
@@ -0,0 +1,12 @@
+import functools
+
+from flask import (
+    Blueprint, flask, g, redirect, render_template, request, url_for
+)
+
+from werkzeug.exceptions import abort
+
+from  flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
